d630e23c3a9f22b580efb3238fb9fefbb63c1df2
Afrie Irham <afrieirham.work@gmail.com>
2023-02-07 22:21:05 +0800

    view: install tailwindcss

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,7 +9,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <p>Hello World</p>
+      <h1 className="text-3xl font-bold underline ">Hello world!</h1>
     </>
   );
 }
